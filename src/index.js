5ca2e68659e594b9c7274c1363fbda7d069fcd98
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2024-07-16 15:57:28 -0300

    Added calcularNivel method to Heroi class

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ class Heroi {
         this.tipo = tipo;
     }
 
+    calcularNivel () {
+        if (this.xp < 1000) {
+            return "Ferro";
+        } else if (this.xp >= 1001 && this.xp <= 2000) {
+            return "Bronze";
+        } else if (this.xp >= 2001 && this.xp <= 5000) {
+            return "Prata";
+        } else if (this.xp >= 5001 && this.xp <= 7000) {
+            return "Ouro";
+        } else if (this.xp >= 7001 && this.xp <= 8000) {
+            return "Platina";
+        } else if (this.xp >= 8001 && this.xp <= 9000) {
+            return "Ascendente";
+        } else if (this.xp >= 9001 && this.xp <= 10000) {
+            return "Imortal";
+        } else {
+            return "Radiante";
+        }
+    }
+
     atacar() {
         let ataque = " ";
 
